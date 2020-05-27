dfc246c4d2832173ebab53a89abeb99a169a5f47
lewis-weinberger <lweinberger@btinternet.com>
2020-05-27 09:56:39 +0100

    updated dwm config with swallow patch
    also added script to configure 9term

--- etc/dwm/6.1/config.h
+++ etc/dwm/6.1/config.h
@@ -1,44 +1,46 @@
-/* See LICENSE file for copyright and license details. */
+/* See NOTICE file for copyright and license details. */
 
 /* appearance */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"B&H LucidaTypewriter:pixelsize=12"
 };
-static const char dmenufont[]       = "monospace:size=10";
-static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#151515";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#ac4142";
-static const char selbgcolor[]      = "#ac4142";
-static const char selfgcolor[]      = "#151515";
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const char dmenufont[]       = "B&H LucidaTypewriter:pixelsize=12";
+static const char normbordercolor[] = "#9EEEEE";
+static const char normbgcolor[]     = "#4E4E4E";
+static const char normfgcolor[]     = "#9EEEEE";
+static const char selbordercolor[]  = "#55AAAA";
+static const char selbgcolor[]      = "#4E4E4E";
+static const char selfgcolor[]      = "#55AAAA";
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 8;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
+	/* class      instance    title       tags mask     isfloating    isterminal    noswallow     monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,            0,            0,           -1 },
+	{ "Firefox",  NULL,       NULL,       0,            0,            0,            0,           -1 },
+	{ "st",       NULL,       NULL,       0,            0,            1,            1,           -1 },
+	{ "9term",    NULL,       NULL,       0,            0,            1,            1,           -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
 };
 
@@ -56,15 +58,18 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *emacscmd[]  = { "emacsclient", "-c",  NULL };
+static const char *termcmd[]  = { "st", "-e", "/usr/bin/rcsh", "-l", NULL };
 static const char *lockcmd[]  = { "xscreensaver-command", "-lock",  NULL };
+static const char *ffcmd[]  = { "firejail", "firefox",  NULL };
+static const char *tbcmd[]  = { "firejail", "thunderbird",  NULL };
+static const char *clkmenucmd[]  = { "9menu.sh",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = emacscmd } },
+	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = ffcmd } },
+	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = tbcmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -114,6 +119,7 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkRootWin,           0,              Button3,        spawn,          {.v = clkmenucmd } },
 };
 
 
